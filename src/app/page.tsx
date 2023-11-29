6ce32e46e40fc094619e9b4ef97dd79e6784c4c5
Gyamfi Dennis <79916056+gyamfidennist@users.noreply.github.com>
2023-11-29 02:39:46 +0000

    Add react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { Button, Text } from '@mantine/core';
+import { Button, Flex, Text } from '@mantine/core';
 import Image from 'next/image';
+import { FcEnteringHeavenAlive } from 'react-icons/fc';
 
 export default function Home() {
   return (
@@ -15,9 +16,13 @@ export default function Home() {
         <Text component="h1" className="text-4xl font-bold text-white mb-2">
           Next.js + Tailwindcss + Mantine
         </Text>
-        <Text component="h2" className="text-2xl font-bold text-white mb-8">
-          A match made in heaven
-        </Text>
+        <Flex align={'center'} mb={'md'} gap={'sm'}>
+          <Text component="h2" className="text-2xl font-bold text-white ">
+            A match made in heaven
+          </Text>
+          <FcEnteringHeavenAlive className="text-3xl" />
+        </Flex>
+
         <Button variant="white" color="black" radius={'xl'} size="lg">
           Get Started
         </Button>
